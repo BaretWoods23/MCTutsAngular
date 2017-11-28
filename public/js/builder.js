615d9846fa292b3d4a7f2f8e35c8e901c7d6b782
Tasokun <tasokun@gmail.com>
2017-11-28 16:51:04 -0700

    .

--- public/js/builder.js
+++ public/js/builder.js
@@ -515,7 +515,30 @@ function writeToJSONFile(){
 	xhr.send(JSON.stringify(jsonObject));
 };
 
+function DataURItoBlob(dataURI) {
+  // convert base64 to raw binary data held in a string
+  // doesn't handle URLEncoded DataURIs
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+  else
+      byteString = unescape(dataURI.split(',')[1]);
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  // write the ArrayBuffer to a blob, and you're done
+  return new Blob([ab],{type: mimeString});
+}
+
 function getLayeredJSONObject(){
+	console.log(DataURItoBlob(imgsrc));
 	var buildName = document.getElementById("buildName").value;
 	var buildUser = document.getElementById("buildUser").value;
 	var cubeArray = getSortedCubeArray();
