b02c374a41703c8ce29ca523c1ec19333f63b9ec
Tasokun <tasokun@gmail.com>
2017-11-29 08:57:35 -0700

    d

--- public/js/builder.js
+++ public/js/builder.js
@@ -482,7 +482,7 @@ window.onload = function(){
 function submit(){
 	locked = true;
 	controls.enabled = false;
-	imgsrc = renderer.domElement.toDataURL("image/bmp", 2);
+	imgsrc = canvas.getContext("2d").getImageData(0, 0, canvWidth, canvHeight).toDataURL();
 	var img = document.getElementById("screenshot")
 	img.src = imgsrc;
 	console.log(imgsrc);
