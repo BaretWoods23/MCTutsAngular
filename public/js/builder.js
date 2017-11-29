18ea85f971ce49cf89f1afa6f5179fdddfa38172
Tasokun <tasokun@gmail.com>
2017-11-28 23:47:44 -0700

    s

--- public/js/builder.js
+++ public/js/builder.js
@@ -482,7 +482,7 @@ window.onload = function(){
 function submit(){
 	locked = true;
 	controls.enabled = false;
-	renderer.setSize(canvWidth/4.5,canvHeight/4.5);
+	renderer.setSize(canvWidth/7,canvHeight/7);
 	imgsrc = renderer.domElement.toDataURL("image/jpeg");
 	var img = document.getElementById("screenshot")
 	img.src = imgsrc;
