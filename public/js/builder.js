339e8388be993f13c4da91c7e8eb85440f4b7703
Tasokun <tasokun@gmail.com>
2017-11-29 14:38:43 -0700

    d

--- public/js/builder.js
+++ public/js/builder.js
@@ -486,8 +486,8 @@ function submit(){
 	var newContext = newCanvas.getContext("2d");
 	newCanvas.height = canvHeight/5;
 	newCanvas.width = canvWidth/5;
-	newContext.drawImage(renderer.domElement,0,0,canvWidth,canvHeight,0,0,canvWidth/2,canvHeight/2);
-	imgsrc = newCanvas.toDataURL("image/jpeg", 0.1);
+	newContext.drawImage(renderer.domElement,0,0,canvWidth,canvHeight,0,0,canvWidth/5,canvHeight/5);
+	imgsrc = newCanvas.toDataURL("image/bmp");
 	var img = document.getElementById("screenshot")
 	img.src = imgsrc;
 	console.log(imgsrc);
