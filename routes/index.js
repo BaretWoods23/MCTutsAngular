f3ea0e1fcc816ca5765628e43b522dba2d6b92b2
Tasokun <tasokun@gmail.com>
2017-11-29 20:58:13 -0700

    s

--- routes/index.js
+++ routes/index.js
@@ -71,7 +71,7 @@ router.get("/builder", ensureAuthenticated, function(req, res){
 
 router.post("/index", function(req, res){
 	var content = JSON.stringify(req.body.content);
-	jsonString = jsonString.concat(content);
+	jsonString = content + jsonString;
 	console.log("HELLO");
 	console.log(jsonString);
 	if(content.includes("}]]}")){
