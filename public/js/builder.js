23e647466f4a77341a937b1c0b5c63140026ce1e
Tasokun <tasokun@gmail.com>
2017-11-29 20:17:45 -0700

    e

--- public/js/builder.js
+++ public/js/builder.js
@@ -530,8 +530,8 @@ function writeToJSONFile(){
 			var xhr = new XMLHttpRequest();
 			xhr.withCredentials = true;
 			xhr.open("POST", "/index");
-			xhr.setRequestHeader("content-type", "application/json;charset=UTF-8");
-			//xhr.setRequestHeader("content-type", "text/plain");
+			//xhr.setRequestHeader("content-type", "application/json;charset=UTF-8");
+			xhr.setRequestHeader("content-type", "text/plain");
 			console.log("TESTING");
 			console.log(jsonString.substring(i-2000,i));
 			xhr.send(jsonString.substring(i-2000,i));
@@ -541,7 +541,7 @@ function writeToJSONFile(){
 	var xhr = new XMLHttpRequest();
 	xhr.withCredentials = true;
 	xhr.open("POST", "/index");
-	xhr.setRequestHeader("content-type", "application/json;charset=UTF-8");
+	xhr.setRequestHeader("content-type", "text/plain");
 	console.log("TESTING");
 	console.log(jsonString.substring(j));
 	xhr.send(jsonString.substring(j));
