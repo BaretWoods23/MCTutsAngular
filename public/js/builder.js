ddb74276ce4023c92bff9789c9c62cdbe6758644
Tasokun <tasokun@gmail.com>
2017-11-29 17:13:56 -0700

    s

--- public/js/builder.js
+++ public/js/builder.js
@@ -529,6 +529,8 @@ function writeToJSONFile(){
 			xhr.withCredentials = true;
 			xhr.open("POST", "/index");
 			xhr.setRequestHeader("content-type", "application/json;charset=UTF-8");
+			console.log("TESTING");
+			console.log(jsonString.substring(i-30,i));
 			xhr.send(jsonString.substring(i-30,i));
 		}
 	}
